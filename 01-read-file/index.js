d3b9b1cbcd120076cc0d943b8d4d9fb53aedf044
BlueOwll <blueowl.n@gmail.com>
2022-05-18 23:29:13 +0300

    feat: task1 complete

---
+++ 01-read-file/index.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const path = require('path');
+const { stdout } = require('process');
+
+const txt_path = path.join(__dirname,'text.txt');
+
+const readStream = fs.createReadStream(txt_path, 'utf-8');
+
+let data = '';
+
+readStream.on('data', (chunk) => {
+  data += chunk;
+  stdout.write(chunk);
+});
+readStream.on('end', () => console.log('End', data));
